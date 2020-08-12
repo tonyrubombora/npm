52e9c59e744ebeab112a4c8587b9bc1d0b8b18c4
= <=>
2020-08-12 21:59:55 +0300

    fixed typo on hostname

--- CORE3/app.js
+++ CORE3/app.js
@@ -1,15 +1,15 @@
 // Creating a server
 
 const http = require('http');
-const port = 3000;
-const hostnam = '127.0.0.1';
+const port = 3001;
+const hostname = '127.0.0.1';
 const server = http.createServer(function(reqest, response) {
     response.statusCode = 200;
     response.end('I am working');
 })
 
 server.listen(port, hostname, () => {
-    console.log(`Server runnign ar http:// ${hostname}:${port}/`)
+    console.log(`Server runnign at http:// ${hostname}:${port}/`)
 })
 
 
